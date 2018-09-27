fc544465d4a3d317a3505c0c9f4c3e1f78742c1d
Alan Tseng <atseng202@gmail.com>
2018-09-27 15:51:43 -0700

    Initial Commit

---
+++ DeckOfCardsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeckOfCardsTest
+//
+//  Created by Alan Tseng on 9/27/18.
+//  Copyright © 2018 atseng202. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
